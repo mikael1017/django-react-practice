fdc26416bf07affd3d8acb507e1c9c25a5f656aa
Jaewoo <jaewoocho1017@gmail.com>
2020-12-08 16:02:39 -0800

    Created a join room page

--- web app/music_controller/api/views.py
+++ web app/music_controller/api/views.py
@@ -13,13 +13,6 @@ class RoomView(generics.CreateAPIView):
     queryset = Room.objects.all()
     serializer_class = RoomSerializer
 
-#  class GetRoom(APIView):
-#     serializer_class = RoomSerializer
-#     lookup_url_kwarg = 'code'
-
-#     def get(self, request, format=None):
-#         code = request.GET.get(self.lookup_url_kwarg)
-
 class GetRoom(APIView):
     serializer_class = RoomSerializer
     lookup_url_kwarg = 'code'
@@ -41,6 +34,7 @@ class CreateRoomView(APIView):
     serializer_class = CreateRoomSerializer
 
     def post(self, request, format = None):
+        #   check if user has active session
         if not self.request.session.exists(self.request.session.session_key):
             self.request.session.create()
 
@@ -54,10 +48,30 @@ class CreateRoomView(APIView):
                 room = queryset[0]
                 room.guest_can_pause = guest_can_pause
                 room.votes_to_skip = votes_to_skip
+                self.request.session['room_code'] = room.code
                 room.save(update_fields = ['guest_can_pause', 'votes_to_skip'])
             else:
                 room = Room(host = host, guest_can_pause = guest_can_pause, votes_to_skip = votes_to_skip)
                 room.save()
+                self.request.session['room_code'] = room.code
             return Response(RoomSerializer(room).data, status = status.HTTP_201_CREATED)
 
         return Response({'Bad Request': 'Invalid data...'}, status = status.HTTP_400_BAD_REQUEST)
+
+
+class JoinRoom(APIView):
+    lookup_url_kwarg = 'code'
+
+    def post(self, request, format = None):
+        if not self.request.session.exists(self.request.session.session_key):
+            self.request.session.create()
+
+        code = request.data.get(self.lookup_url_kwarg)
+        if code != None:
+            room_result = Room.objects.filter(code = code)
+            if len(room_result) > 0:
+                room = room_result[0]
+                self.request.session['room_code'] = code
+                return Response({'message': 'Room Joined!'}, status=status.HTTP_200_OK)
+            return Response({'Bad Request': 'Invalid Room Code'}, status=status.HTTP_400_BAD_REQUEST)
+        return Response({'Bad Request': 'Invalid post data, did not find a code key'}, status = status.HTTP_400_BAD_REQUEST)
